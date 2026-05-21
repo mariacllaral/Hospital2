60ecd6bfff609833667681ea8f6d114ff6372eb1
Washington <washingtoncip@outlok.com>
2026-05-21 17:01:40 -0300

    Criação do menu e da classe Paciente

---
+++ Paciente.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital
+{
+    internal class Paciente
+    {
+        public string nome;
+        public int idade;
+        public string sintoma;
+        public string telefone;
+        public bool preferencial;
+
+        public void cadastrarPaciente()
+        {
+            Console.WriteLine("Digite seu nome:");
+            this.nome = Console.ReadLine();
+            while (this.nome == "")
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("Informação Inválida digite novamente\n");
+                Console.ResetColor();
+                Console.WriteLine("Digite seu nome:");
+                this.nome = Console.ReadLine();
+            }
+
+            Console.WriteLine("Digite sua Idade:");
+            string inputIdade = Console.ReadLine();
+            while (!int.TryParse(inputIdade, out this.idade))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("Informação Inválida digite novamente\n");
+                Console.ResetColor();
+                Console.WriteLine("Digite sua Idade");
+                inputIdade = Console.ReadLine();
+                int.TryParse(inputIdade, out this.idade);
+            }
+
+            Console.WriteLine("Paciente preferencial? (s/n):");
+            string resp = Console.ReadLine().ToLower();
+
+            this.preferencial = resp == "s";
+
+            Console.WriteLine("Telefone com DDD(apenas numeros):");
+            Console.WriteLine("EX:11970707070");
+            string tel = Console.ReadLine();
+            while (tel.Length != 11)
+            {
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("Informação Inválida digite novamente\n");
+                Console.ResetColor();
+                Console.WriteLine("Telefone com DDD(apenas numeros):");
+                Console.WriteLine("EX:11970707070");
+                tel = Console.ReadLine();
+            }
+            if (tel.Length == 11)
+            {
+                this.telefone = tel.Insert(0, "(").Insert(3, ")").Insert(9, "-");
+            }
+
+            Console.WriteLine("Quais sintomas o paciente apresenta!");
+            this.sintoma = Console.ReadLine();
+            while (this.sintoma == "")
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("Informação Inválida digite novamente\n");
+                Console.ResetColor();
+                Console.WriteLine("Digite seu nome:");
+                this.sintoma = Console.ReadLine();
+            }
+
+         
+        }
+       // public virtual void mostraDados()
+      //  {
+       //     Console.WriteLine("Seus dados:\n");
+       //     Console.WriteLine("Nome: {0}\nIdade: {1}\nPrefencial: {2}\nTelefone: {3}\nSintoma: {4}", this.nome, this.idade,this.preferencial, this.telefone, this.sintoma);
+       // }
+    }
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string op = "";
+            Paciente p = new Paciente();
+
+            while (op.ToUpper() != "q")
+            {
+                Console.WriteLine("--------------------------------------");
+                Console.WriteLine("| Opção desejada                     |");
+                Console.WriteLine("| Cadastrar Paciente -- 1            |");
+                Console.WriteLine("| Listar Pacientes ---- 2            |");
+                Console.WriteLine("| Atender Paciente ---- 3            |");
+                Console.WriteLine("| Fechar -------------- Q            |");
+                Console.WriteLine("--------------------------------------");
+                Console.Write("| Digite o numero da opção: ");
+                op = Console.ReadLine();
+                Console.WriteLine("--------------------------------------");
+                switch (op.ToUpper())
+                {
+                    case "1":
+                        p.cadastrarPaciente();
+                        Console.Clear();
+                        break;
+
+                    case "2":
+                        p.mostraDados();
+                        break;
+
+                    case "3":
+                        
+                        break;
+
+                    case "Q":
+                        Console.WriteLine("Saindo do sistema...");
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção inválida!");
+                        Console.Clear();
+                        break;
+                }
+            }
+        }
+    }
+}
+
+
